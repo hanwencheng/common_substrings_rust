9cff2912f396664ed35347b9c8a76e6dd5b10017
Hanwen Cheng <heawen.cheng@gmail.com>
2019-12-28 22:21:39 +0100

    build node with prefix of string

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,28 @@
-use std::collections::{HashMap, BTreeMap};
+use std::collections::BTreeMap;
+use std::fmt::{Debug, Formatter};
+use std::fmt;
 
 const MIN_LENGTH: usize = 3;
-const MIN_OCCURRENCES: u32 = 2;
+const MIN_OCCURRENCES: usize = 2;
 
 struct Node {
-    chars: HashMap<char, Node>,
-    val: i32,
-    horizontal: usize,
+    source: Option<Vec<usize>>,
+    listed: Option<Vec<usize>>,
+}
+
+impl Node {
+    pub fn new(char: &str, source_index: usize) -> Node {
+        Node {
+            source: Some(vec![source_index]),
+            listed: Some(vec![]),
+        }
+    }
+}
+
+impl Debug for Node {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "(source: {:?}, listed: {:?})", self.source, self.listed)
+    }
 }
 
 struct Trie {
@@ -23,25 +39,25 @@ pub fn add(a: i32, b: i32) -> i32 {
 
 pub fn build_array(input: Vec<&str>) {
 //    let mut stucture: Structure = {};
-    let mut movie_reviews= BTreeMap::new();
-    movie_reviews.insert("Office Space",       "Deals with real issues in the workplace.");
+    let mut trie: BTreeMap<String, Node> = BTreeMap::new();
     for x in 0..10 {
         println!("{}", x); // x: i32
     }
-    for word in input.iter() {
+    for (word_index, word) in input.iter().enumerate() {
         println!("word: {}", word);
-        build_string(&word);
+        build_string(&word, &mut trie, word_index);
     }
 }
 
-pub fn build_string(word: &str) {
+fn build_string(word: &str, trie: &mut BTreeMap<String, Node>, word_index: usize) {
     let mut iter = word.chars().skip(MIN_LENGTH - 1).enumerate();
     while let Some((index, char)) = iter.next() {
+        let char_label = char.to_string();
+        let insert_node = Node::new(&char_label, word_index);
+        let insert_result = trie.insert(char_label, insert_node);
+        if let Some(node) = insert_result {
+            println!("char insert node {:?}", node);
+        }
         println!("char: {}, at index {}", char, index + MIN_LENGTH);
     }
 }
-
-fn build_trie(input: Vec<&str>) {}
-
-
-
