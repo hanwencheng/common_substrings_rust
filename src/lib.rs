2a0a80c8e4c8e3465f19d8a3584dc00cb7e83bc7
Hanwen Cheng <heawen.cheng@gmail.com>
2020-01-07 00:19:03 +0100

    use set and iteration tree

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::{HashMap, HashSet};
 use std::fmt::{Debug, Formatter};
 use std::fmt;
 
@@ -6,15 +6,26 @@ const MIN_LENGTH: usize = 3;
 const MIN_OCCURRENCES: usize = 2;
 
 struct Node {
-    source: Vec<usize>,
+    source: HashSet<usize>,
     listed: bool,
+    nodes: HashMap<String, Node>
 }
 
 impl Node {
-    pub fn new(char: &str, source_index: usize) -> Node {
+    pub fn new(source_index: usize) -> Node {
+        let mut source:HashSet<usize> = HashSet::new();
+        source.insert(source_index);
         Node {
-            source: vec![source_index],
+            source,
             listed: false,
+            nodes: HashMap::new()
+        }
+    }
+    pub fn root() -> Node {
+        Node {
+            source: HashSet::new(),
+            listed: false,
+            nodes: HashMap::new()
         }
     }
 }
@@ -38,25 +49,27 @@ pub fn add(a: i32, b: i32) -> i32 {
 }
 
 pub fn build_array(input: Vec<&str>) {
-//    let mut stucture: Structure = {};
-    let mut trie: BTreeMap<String, Node> = BTreeMap::new();
+    let mut trie= Node::root();
     for (word_index, word) in input.iter().enumerate() {
         println!("word: {}", word);
         build_string(&word, &mut trie, word_index);
     }
 }
 
-fn build_string(word: &str, trie: &mut BTreeMap<String, Node>, word_index: usize) {
-    let mut iter = word.chars().skip(MIN_LENGTH - 1).enumerate();
-    while let Some((index, char)) = iter.next() {
-        let char_label = char.to_string();
-        let insert_node = Node::new(&char_label, word_index);
-        let contains_key = trie.contains_key(&char_label);
-        let current_node = trie.entry(char_label).or_insert(insert_node);
-        &current_node.source.push(word_index);
-//        if let Some(list) = &mut current_node.listed{
-//            list.push(word_index);
-//        }
-        println!("char: {}, at index {}, {:?}", char, index + MIN_LENGTH, &current_node.source);
+fn build_string(word: &str, trie: &mut Node, word_index: usize) {
+    let mut pointer = trie;
+    for x in 0 .. word.len() - MIN_LENGTH + 1 {
+        let substring = &word[x..];
+        println!("suffix is {}", substring);
+        let mut iter = substring.chars().skip(MIN_LENGTH - 1).enumerate();
+        while let Some((index, char)) = iter.next() {
+            let char_label = char.to_string();
+            let insert_node = Node::new(word_index);
+            let contains_key = pointer.nodes.contains_key(&char_label);
+            let current_node = pointer.nodes.entry(char_label).or_insert(insert_node);
+            &current_node.source.insert(word_index);
+            pointer = current_node;
+            println!("char: {}, at index {}, {:?}", char, index + MIN_LENGTH, &pointer.source);
+        }
     }
 }
