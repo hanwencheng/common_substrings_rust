17ee38a3d90ea452762db006ba40155e7e0d8a3e
Hanwen Cheng <heawen.cheng@gmail.com>
2020-01-08 23:23:33 +0100

    build horizontal trie

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,8 @@ const MIN_OCCURRENCES: usize = 2;
 struct Node {
     source: HashSet<usize>,
     listed: bool,
-    nodes: HashMap<String, Node>
+    nodes: HashMap<String, Node>,
+    horizontal: HashMap<String, *mut Node>,
 }
 
 impl Node {
@@ -20,14 +21,15 @@ impl Node {
         Node {
             source: HashSet::new(),
             listed: false,
-            nodes: HashMap::new()
+            nodes: HashMap::new(),
+            horizontal: HashMap::new(),
         }
     }
 }
 
 impl Debug for Node {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "(source: {:?}, listed: {:?})", self.source, self.listed)
+        write!(f, "(source: {:?}, listed: {:?}), horizontal: {:?}", self.source, self.listed, self.horizontal)
     }
 }
 
@@ -44,14 +46,17 @@ pub fn add(a: i32, b: i32) -> i32 {
 }
 
 pub fn build_array(input: Vec<&str>) {
-    let mut trie= Node::new();
+    let mut trie = Node::new();
+    let mut horizontal_trie_root: HashMap<String, *mut Node> = HashMap::new();
     for (word_index, word) in input.iter().enumerate() {
         println!("word: {}", word);
-        build_string(&word, &mut trie, word_index);
+        build_suffices(&word, &mut trie, word_index, &mut horizontal_trie_root);
     }
+    println!("horizontal root is {:?}", horizontal_trie_root);
 }
 
-fn build_suffix(substring: &str, trie: &mut Node, word_index: usize) {
+fn build_suffix(substring: &str, trie: &mut Node, word_index: usize, last_suffix_leaf: *mut Node)
+                -> *mut Node {
     let mut pointer = trie;
     println!("suffix is {}", substring);
     let mut iter = substring.chars().skip(MIN_LENGTH - 1).enumerate();
@@ -59,18 +64,28 @@ fn build_suffix(substring: &str, trie: &mut Node, word_index: usize) {
         let char_label = char.to_string();
         let insert_node = Node::new();
         let contains_key = pointer.nodes.contains_key(&char_label);
-        println!("is contained {}", contains_key);
         let current_node = pointer.nodes.entry(char_label).or_insert(insert_node);
         if index + MIN_LENGTH == substring.len() {
             current_node.update(word_index);
+            if index != 0 {
+                unsafe {
+                    (*last_suffix_leaf).horizontal.insert(String::from(substring), current_node as *mut Node);
+                    println!("last_suffix_leaf updated {:?}", *last_suffix_leaf)
+                };
+            }
         }
         pointer = current_node;
-        println!("char: {}, at index {}, {:?}", char, index + MIN_LENGTH, &pointer.source);
+        println!("char: {}, at index {}", char, index + MIN_LENGTH);
     }
+
+    return pointer as *mut Node;
 }
 
-fn build_string(word: &str, trie: &mut Node, word_index: usize) {
-    for x in 0 .. word.len() - MIN_LENGTH + 1 {
-        build_suffix(&word[x..], trie, word_index);
+fn build_suffices(word: &str, trie: &mut Node, word_index: usize, horizontal_root: &mut HashMap<String, *mut Node>) {
+    if word.len() < MIN_LENGTH { return; }
+    let mut last_suffix_leaf: *mut Node = &mut Node::new() as *mut Node;
+    for x in 0..word.len() - MIN_LENGTH + 1 {
+        last_suffix_leaf = build_suffix(&word[x..], trie, word_index, last_suffix_leaf);
     }
+    horizontal_root.insert(String::from(&word[word.len() - 3..]), last_suffix_leaf);
 }
