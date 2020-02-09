bc7633bf45b9c627e14902fc992b424269589936
Hanwen Cheng <heawen.cheng@gmail.com>
2020-02-09 14:02:06 +0100

    complete accumulate function

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ struct Node {
 }
 
 impl Node {
-    pub fn update(&mut self, source_index: usize) {
+    pub fn add_source(&mut self, source_index: usize) {
         self.source.insert(source_index);
     }
 
@@ -29,7 +29,7 @@ impl Node {
 
 impl Debug for Node {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "(source: {:?}, listed: {:?}), horizontal: {:?}", self.source, self.listed, self.horizontal)
+        write!(f, "(source: {:?}, listed: {:?}), horizontal: {:?} with Map {:?}", self.source, self.listed, self.horizontal, self.nodes)
     }
 }
 
@@ -37,15 +37,27 @@ struct Trie {
     root: Node
 }
 
-struct Structure {}
-
-struct HorizontalStructure {}
-
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 
-pub fn build_array(input: Vec<&str>) {
+/* main entry */
+pub fn get_substrings (input: Vec<&str>) {
+    let (mut built_trie, mut horizontal_root) = build_array(input);
+}
+
+fn accumulate_source(trie: &mut Node) -> HashSet<usize> {
+    let accumulated_sources =  trie.nodes.iter_mut().fold(trie.source.clone(), |acc:HashSet<usize>, (child_node_label, child_node)| {
+        let child_sources = accumulate_source(child_node);
+        return acc.union(&child_sources).cloned().collect();
+    });
+    if trie.source.len() != 0 {
+        trie.source = accumulated_sources.clone();
+    }
+    return accumulated_sources;
+}
+
+fn build_array(input: Vec<&str>) -> (Node, HashMap<String, *mut Node>) {
     let mut trie = Node::new();
     let mut horizontal_trie_root: HashMap<String, *mut Node> = HashMap::new();
     for (word_index, word) in input.iter().enumerate() {
@@ -53,10 +65,14 @@ pub fn build_array(input: Vec<&str>) {
         build_suffices(&word, &mut trie, word_index, &mut horizontal_trie_root);
     }
     println!("horizontal root is {:?}", horizontal_trie_root);
+    println!("trie root is {:?}", trie);
+    accumulate_source(&mut trie);
+    return (trie, horizontal_trie_root);
 }
 
-fn build_suffix(substring: &str, trie: &mut Node, word_index: usize, last_suffix_leaf: *mut Node)
+fn build_suffix(string: &str, trie: &mut Node, word_index: usize, last_suffix_leaf: *mut Node, substring_index: usize)
                 -> *mut Node {
+    let substring = &string[substring_index..];
     let mut pointer = trie;
     println!("suffix is {}", substring);
     let mut iter = substring.chars().skip(MIN_LENGTH - 1).enumerate();
@@ -66,11 +82,11 @@ fn build_suffix(substring: &str, trie: &mut Node, word_index: usize, last_suffix
         let contains_key = pointer.nodes.contains_key(&char_label);
         let current_node = pointer.nodes.entry(char_label).or_insert(insert_node);
         if index + MIN_LENGTH == substring.len() {
-            current_node.update(word_index);
-            if index != 0 {
+            current_node.add_source(word_index);
+            if substring_index > 0 {
                 unsafe {
-                    (*last_suffix_leaf).horizontal.insert(String::from(substring), current_node as *mut Node);
-                    println!("last_suffix_leaf updated {:?}", *last_suffix_leaf)
+                    current_node.horizontal.insert(String::from(&substring[(substring_index-1)..]), last_suffix_leaf as *mut Node);
+                    println!("last_suffix_leaf updated {:?}", current_node)
                 };
             }
         }
@@ -85,7 +101,7 @@ fn build_suffices(word: &str, trie: &mut Node, word_index: usize, horizontal_roo
     if word.len() < MIN_LENGTH { return; }
     let mut last_suffix_leaf: *mut Node = &mut Node::new() as *mut Node;
     for x in 0..word.len() - MIN_LENGTH + 1 {
-        last_suffix_leaf = build_suffix(&word[x..], trie, word_index, last_suffix_leaf);
+        last_suffix_leaf = build_suffix(&word, trie, word_index, last_suffix_leaf, x);
     }
     horizontal_root.insert(String::from(&word[word.len() - 3..]), last_suffix_leaf);
 }
